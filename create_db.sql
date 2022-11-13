7ae8fce2d1c69b8b309acee3fb6c6ce9feb07046
pennamoro <gpennamoro@gmail.com>
2022-11-13 14:43:39 -0300

    corrected scripts db

--- create_db.sql
+++ create_db.sql
@@ -5,47 +5,80 @@ CREATE DATABASE lavanderia
     CONNECTION LIMIT = -1
 ;
 
+CREATE TABLE tb_user(
+  id_user SERIAL,
+  id_endereco NUMBER NOT NULL,
+  cpf_user CHAR(11) NOT NULL UNIQUE,
+  nome_user VARCHAR(255) NOT NULL,
+  email_user VARCHAR(255) NOT NULL UNIQUE,
+  telefone_user VARCHAR(30) NOT NULL,
+  senha_user CHAR(64) NOT NULL,
+  tipo_user INTEGER NOT NULL,
+  CONSTRAINT pk_user PRIMARY KEY (id_user)
+  CONSTRAINT fk_endereco_u FOREIGN KEY (id_endereco) REFERENCES endereco(id_endereco);
+);
+
+CREATE TABLE tb_pedido(
+  id_pedido SERIAL,
+  descricao_pedido VARCHAR(255) NOT NULL,
+  orcamento_pedido NUMBER NOT NULL,
+  CONSTRAINT pk_pedido PRIMARY KEY (id_pedido)
+);
+
+CREATE TABLE tb_item(
+  id_item SERIAL,
+  descricao_item VARCHAR(255) NOT NULL,
+  preco_uni NUMBER NOT NULL,
+  tempo NUMBER NOT NULL,
+  CONSTRAINT pk_item PRIMARY KEY (id_item)
+);
 
 CREATE TABLE tb_estado (
   id_estado SERIAL PRIMARY KEY,
   nome_estado VARCHAR(50),
   uf_estado CHAR(2)
+  CONSTRAINT pk_estado PRIMARY KEY (id_estado)
 );
-
 
 CREATE TABLE tb_cidade (
   id_cidade SERIAL PRIMARY KEY,
-  nome_cidade VARCHAR(255),
   id_estado INTEGER,
-  CONSTRAINT fk_cidade_estado FOREIGN KEY (id_estado) REFERENCES tb_estado (id_estado)
+  nome_cidade VARCHAR(255),
+  CONSTRAINT pk_cidade PRIMARY KEY (id_cidade)
+  CONSTRAINT fk_cidade_es FOREIGN KEY (id_estado) REFERENCES tb_estado (id_estado)
 );
-
 
 CREATE TABLE tb_endereco (
   id_endereco SERIAL PRIMARY KEY,
-  cep CHAR(8) NOT NULL,
   id_cidade INTEGER NOT NULL,
+  cep CHAR(8) NOT NULL,
   logradouro VARCHAR(255) NOT NULL,
   numero INTEGER NOT NULL,
   complemento VARCHAR(255),
   bairro VARCHAR(255) NOT NULL,
-  CONSTRAINT fk_cidade_endereco FOREIGN KEY (id_cidade) REFERENCES tb_cidade (id_cidade)
+  CONSTRAINT fk_cidade_en FOREIGN KEY (id_cidade) REFERENCES tb_cidade(id_cidade)
 );
 
-
-CREATE TABLE tb_user (
-  id_user SERIAL PRIMARY KEY,
-  cpf_user CHAR(11) NOT NULL UNIQUE,
-  nome_user VARCHAR(255) NOT NULL,
-  email_user VARCHAR(255) NOT NULL UNIQUE,
-  id_endereco INTEGER,
-  tel_user VARCHAR(30) NOT NULL,
-  pw_user VARCHAR(255) NOT NULL,
-  tipo_user VARCHAR(11),
-  CONSTRAINT fk_user_endereco FOREIGN KEY (id_endereco) REFERENCES tb_endereco (id_endereco)
+CREATE TABLE user_has_pedido(
+  id_user NUMBER NOT NULL,
+  id_pedido NUMBER NOT NULL,
+  status VARCHAR(255) NOT NULL,
+  data_inicio DATE NOT NULL,
+  data_fim DATE NOT NULL,
+  CONSTRAINT pk_uhp PRIMARY KEY (id_user, id_pedido),
+  CONSTRAINT fk_user_p FOREIGN KEY (id_user) REFERENCES tb_user(id_user),
+  CONSTRAINT fk_pedido_u FOREIGN KEY (id_pedido) REFERENCES tb_pedido(id_user)
 );
 
-INSERT INTO tb_user (cpf_user, nome_user, email_user, tel_user, pw_user, tipo_user) VALUES ('12332112345', 'Testôncíò Testônildo da Silva', 'teste@teste.ufpr.br', '+5541694209420', '289160db0d9f39f9ae1754c4ec9c16f90b50e32e09c5fb5481ae642b3d3d1a36', 'cliente');
+CREATE TABLE pedido_has_item(
+  id_pedido NUMBER NOT NULL,
+  id_item NUMBER NOT NULL,
+  CONSTRAINT pk_phi PRIMARY KEY (id_pedido, id_item),
+  CONSTRAINT fk_pedido_i FOREIGN KEY (id_pedido) REFERENCES tb_pedido(id_pedido),
+  CONSTRAINT fk_item_p FOREIGN KEY (id_item) REFERENCES tb_item(id_pedido),
+);
+
+INSERT INTO tb_user (endereco_user, cpf_user, nome_user, email_user, telefone_user, senha_user, tipo_user) VALUES ('1','12332112345', 'Testôncíò Testônildo da Silva', 'teste@teste.ufpr.br', '+5541694209420', '289160db0d9f39f9ae1754c4ec9c16f90b50e32e09c5fb5481ae642b3d3d1a36', 'cliente');
 
 INSERT INTO tb_estado (id_estado, nome_estado, uf_estado) VALUES
 (1, 'Acre', 'AC'),
@@ -5660,3 +5693,4 @@ INSERT INTO tb_cidade (id_cidade, nome_cidade, id_estado) VALUES
 (5579, 'Wanderlândia', 27),
 (5580, 'Xambioá', 27)
 ;
+
