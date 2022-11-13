55eb7211e9ced19e4c69c559076f952b214802b9
Gianluca Magnabosco <gianluca_1406@hotmail.com>
2022-11-13 13:53:39 -0300

    email message htmlPage

--- src/java/util/MailService.java
+++ src/java/util/MailService.java
@@ -46,12 +46,14 @@ public class MailService {
     
     private static Message prepareMessage(Session session, String userEmail, String userPassword) {
         Message message = new MimeMessage(session);
-       
+        
+        String htmlPage = "<!DOCTYPE html><html><head><meta content=\"text/html; charset=utf-8\" http-equiv=\"Content-Type\"><title>Sua senha</title></head><body marginheight=\"0\" topmargin=\"0\" marginwidth=\"0\" style=\"margin:0;background-color:#f2f3f8\" leftmargin=\"0\"><table cellspacing=\"0\" border=\"0\" cellpadding=\"0\" width=\"100%\" bgcolor=\"#f2f3f8\" style=\"@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700);font-family:'Open Sans',sans-serif\"><tr><td><table style=\"background-color:#f2f3f8;max-width:670px;margin:0 auto\" width=\"100%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\"><tr><td style=\"height:80px\">&nbsp;</td></tr><tr><td style=\"text-align:center\"><img style=\"border-width:0;width:250px;height:160px;margin-bottom:15px;margin-top:10px\" src=\"https://media.discordapp.net/attachments/872237870109298760/1041390707631460532/logo-icone.png\" title=\"logo\" alt=\"Lavanderia On-Line\"></td></tr><tr><td style=\"height:20px\">&nbsp;</td></tr><tr><td><table width=\"95%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" style=\"max-width:670px;background:#fff;border-radius:3px;text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06)\"><tr><td style=\"height:40px\">&nbsp;</td></tr><tr><td style=\"padding:0 35px\"><h1 style=\"color:#1e1e2d;font-weight:500;margin:0;font-size:32px;font-family:Rubik,sans-serif\">Seja bem-vindo!</h1><span style=\"display:inline-block;vertical-align:middle;margin:29px 0 26px;border-bottom:1px solid #cecece;width:100px\"></span><p style=\"color:#455056;font-size:15px;line-height:24px;margin:0\">Você foi cadastrado com sucesso no sistema LOL - Lavanderia Online.<br>Estamos enviando neste e-mail a senha que você utilizará para acessar o sistema.<br>Sua senha atual é:<br><b>" + userPassword + "</b><br>Utilize seu e-mail e a senha acima para acessar o sistema. Seja muito bem-vindo!</p></td></tr><tr><td style=\"height:40px\">&nbsp;</td></tr></table></td><tr><td style=\"height:20px\">&nbsp;</td></tr><tr><td style=\"text-align:center\"><p style=\"font-size:14px;color:rgba(69,80,86,.7411764705882353);line-height:18px;margin:0 0 0\">&copy;<strong>LOL - Lavanderia Online</strong></p></td></tr><tr><td style=\"height:80px\">&nbsp;</td></tr></table></td></tr></table></body></html>";
+        
         try {
             message.setFrom(new InternetAddress(EMAIL_ADDRESS));
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(userEmail));
             message.setSubject("Confirmação de cadastro - LOL Lavanderia Online");
-            message.setText("Olá tudo bem?\n\nSua conta no sistema LOL - Lavanderia Online foi criada com sucesso!\n\nPara acessar o sistema utilize seu e-mail e a senha a seguir: " + userPassword);
+            message.setContent(htmlPage, "text/html");
             return message;
         } catch (Exception e) {
             e.printStackTrace();
